5d25f8072e0481c4c5e5b324977e24082a594aff
JohnRFoster <fosterj@bu.edu>
2025-11-06 12:32:32 -0700

    known states for mna

--- R/functions.R
+++ R/functions.R
@@ -231,3 +231,23 @@ capture_matrix <- function(site, neon.smam) {
 		delta.days = delta.days
 	))
 }
+
+#' Known states function
+#'
+#' This function fills in the capture history matrix where individuals are known but not observed
+#' @param ch Capture history matrix
+#' @export
+#' @examples known_states(ch)
+
+known_states <- function(ch) {
+	state <- ch
+	for (i in 1:dim(ch)[1]) {
+		n1 <- min(which(ch[i, ] != 0))
+		n2 <- max(which(ch[i, ] != 0))
+		if (n2 > n1) {
+			state[i, n1:n2] <- 1
+		}
+		#state[i, n1:n2] <- 1
+	}
+	return(state)
+}
